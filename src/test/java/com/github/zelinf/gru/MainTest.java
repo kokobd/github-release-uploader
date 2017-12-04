174c11f32e2385aee9a70704e570613fb098c079
Zelin Feng <fengzlin@mail2.sysu.edu.cn>
2017-12-04 22:17:12 +0800

    Implement integration test

--- src/test/java/com/github/zelinf/gru/MainTest.java
+++ src/test/java/com/github/zelinf/gru/MainTest.java
@@ -1,58 +1,140 @@
 package com.github.zelinf.gru;
 
+import com.jcabi.github.*;
+import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.Properties;
-
-import static org.junit.Assert.*;
 
 public class MainTest {
 
+    private final String authToken;
+    private final String username;
+    private final String repoName;
+
     public MainTest() throws IOException {
-        config = new Properties();
-        config.load(MainTest.class.getResourceAsStream("test-config.properties"));
+        Properties config = new Properties();
+        config.load(MainTest.class.getResourceAsStream("/resources/test-config.properties"));
+
+        authToken = config.getProperty("authToken");
+        username = config.getProperty("username");
+        repoName = config.getProperty("repoName");
+
+        Github github = new RtGithub(authToken);
+        remoteRepository = github.repos().get(new Coordinates.Simple(username, repoName));
     }
 
     @Before
-    void setUp() throws Exception {
-        Path tempDir = Files.createTempDirectory("gru-test");
+    public void setUp() throws Exception {
+        cloneRepo();
+        createUploadFile();
 
-        try (Git git = cloneRepo()) {
+        deleteRelease();
+    }
+
+    private Path testDir; // repoDir is placed inside testDir
+    private Path repoDir;
+
+    private void createUploadFile() throws IOException {
+        final String fileName = "test-archive.zip";
+        uploadFile = repoDir.resolve(fileName);
+        Files.copy(MainTest.class.getResourceAsStream("/resources/" + fileName),
+                uploadFile, StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    private Path uploadFile;
+
+    private final String tag = "v0.1.0";
+
+    @Test
+    public void main() throws IOException {
+        Main.main("--token", authToken,
+                "--repo-dir", repoDir.toString(),
+                "--file", uploadFile.toString(),
+                "--mime-type", "application/zip",
+                "--tag", tag);
+
+        expectsReleaseAsset(uploadFile.getFileName().toString(),
+                uploadFile);
+    }
+
+    private Repo remoteRepository;
+
+    private void expectsReleaseAsset(String assetName, Path assetFile) throws IOException {
+        Iterable<Release> releases = remoteRepository.releases().iterate();
+
+        boolean foundRelease = false;
+        for (Release release : releases) {
+            if (foundRelease)
+                break;
+
+            Release.Smart smartRelease = new Release.Smart(release);
+            if (smartRelease.tag().equals(tag)) {
+                foundRelease = true;
+                boolean foundAsset = false;
+
+                Iterable<ReleaseAsset> assets = smartRelease.assets().iterate();
+                for (ReleaseAsset someAsset : assets) {
+                    if (foundAsset)
+                        break;
+
+                    ReleaseAsset.Smart releaseAsset = new ReleaseAsset.Smart(someAsset);
+                    if (releaseAsset.name().equals(assetName)) {
+                        foundAsset = true;
+
+                        byte[] fileContent = Files.readAllBytes(assetFile);
+                        byte[] assetContent = new byte[fileContent.length];
+                        //noinspection ResultOfMethodCallIgnored
+                        releaseAsset.raw().read(assetContent);
+
+                        Assert.assertArrayEquals(fileContent, assetContent);
+                    }
+                }
+
+                Assert.assertTrue(foundAsset);
+            }
+        }
+
+        Assert.assertTrue(foundRelease);
+    }
+
+    private void deleteRelease() {
+        Releases.Smart releases = new Releases.Smart(remoteRepository.releases());
+        try {
+            Release release = releases.find(tag);
+            release.delete();
+        } catch (IOException ignored) {
         }
     }
 
-    private Path repoDir;
-
-    @Test
-    void main() {
-        Main.main("--token", "abc",
-                "--file", "path",
-                "--mime-type", "application/zip",
-                "--tag", "v0.1.0");
-    }
-
     @After
-    void tearDown() throws Exception {
-
+    public void tearDown() throws Exception {
+        deleteRelease();
+        FileUtils.deleteDirectory(testDir.toFile());
     }
 
-    private Properties config;
+    private void cloneRepo() throws GitAPIException, IOException {
+        testDir = Files.createTempDirectory("gru-test");
+        repoDir = testDir.resolve(repoName);
 
-    private Git cloneRepo() throws GitAPIException {
         String uri = String.format("https://github.com/%s/%s",
-                config.getProperty("username"),
-                config.getProperty("repoName"));
+                username,
+                repoName);
 
-        return Git.cloneRepository()
+        //noinspection EmptyTryBlock
+        try (Git ignored = Git.cloneRepository()
                 .setURI(uri)
-                .call();
+                .setDirectory(repoDir.toFile())
+                .call()) {
+        }
     }
 }
